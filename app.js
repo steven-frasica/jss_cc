1883cb466bff488bd222c9c0db956e6a1d5bfb0d
steven-frasica <steven.frasica@gmail.com>
2026-03-17 15:32:52 -0400

    add function section

--- app.js
+++ app.js
@@ -85,21 +85,105 @@ console.log(bool);
 //   console.log(`not enough money - you still owe ${difference * -1} dollars`);
 // }
 
-
 // Ternary Operators
 
 let hot = true;
 
-hot ? console.log('weather is hot outside') : console.log('weather is cold')
+hot ? console.log("weather is hot outside") : console.log("weather is cold");
 
-let subscribed = false
-let loggedIn = true
-let string = subscribed || loggedIn ? 'show the video' : 'hide the video'
+let subscribed = false;
+let loggedIn = true;
+let string = subscribed || loggedIn ? "show the video" : "hide the video";
 console.log(string);
 
-let cash = 50
-let price = 40
-let isStoreOpen = true
+let cash = 50;
+let price = 40;
+let isStoreOpen = true;
 
-let receiptString = cash >= price && isStoreOpen ? 'give receipt' : 'do not give receipt'
-console.log(receiptString)
+let receiptString =
+  cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
+console.log(receiptString);
+
+// Loops
+
+// DRY = Don't repeat yourself
+let count = 1;
+
+while (count <= 100) {
+  console.log(count);
+  count = count + 1;
+}
+
+for (let i = 0; i <= 3; i++) {
+  console.log(i);
+}
+
+console.log("fizzbuzz problem starts here");
+
+for (i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`);
+  } else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`);
+  } else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`);
+  } else {
+    console.log(`${i} -> ${i}`);
+  }
+}
+
+console.log("print every letter in the string");
+
+const loopString = "Frontend Simplified";
+
+for (i = 0; i < loopString.length; i++) {
+  console.log(loopString[i]);
+}
+
+// console.log("Welcome to FES, Steven")
+// console.log("Welcome to FES, Aïcha")
+// console.log("Welcome to FES, Mari")
+
+const names = ["Steven", "Aïcha", "Mari"];
+
+// for(i = 0; i < names.length; i++) {
+//   console.log(`Welcome to FES, ${names[i]}`);
+// }
+
+const welcome = (name) => console.log(`Welcome to FES, ${name}`);
+
+console.log(welcome(names[1]));
+
+// Function definition
+function welcomePersonToFES(name, lastName) {
+  console.log(`Welcome to Frontend Simplified, ${name} ${lastName}`);
+}
+
+// Call the function
+welcomePersonToFES("Steven", "Frasica");
+
+function fn() {
+  return 5;
+  console.log("my function");
+}
+
+console.log(fn());
+
+function sumOfTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sumOfTwoNumbers(10, 20));
+
+// function celsiusToFahrenheit(celsius) {
+//   return celsius * 1.8 + 32;
+// }
+
+// console.log(`Temperature is ${celsiusToFahrenheit(10)} degrees Fahrenheit`);
+
+const celsiusToFahrenheit = celsius => celsius * 1.8 + 32;
+
+console.log(`Temperature is ${celsiusToFahrenheit(10)} degrees Fahrenheit`);
+
+// ARRAYS
+
